0f7882434b1bf446e09ce3513dd07d33839737fb
Juan Manuel Servera <8036360+jmservera@users.noreply.github.com>
2021-12-14 21:23:06 +0000

    day 8 - challenge 2, dirty trick

--- day07/GiantWhale/Crab.cs
+++ day07/GiantWhale/Crab.cs
@@ -9,6 +9,7 @@ namespace Day07.GiantWhale
         public Crab(int[] data){
             this.data=data;
         }
+
         public int Solve(){
             var groups = data.GroupBy(v => v);
             int maxCount = groups.Max(g => g.Count());
@@ -16,9 +17,28 @@ namespace Day07.GiantWhale
 
             int sum = int.MaxValue;
             foreach(var m in mode){
-                sum=Math.Min(sum,data.Sum(a => a<m?m-a:a-m));
+                sum=Math.Min(sum,data.Sum(a => Math.Abs(m-a)));
             }
             return sum;
+        }
+
+        public int IncreasingPriceSolve()
+        {
+            var groups = data.GroupBy(v => v);
+            int maxCount = groups.Max(g => g.Count());
+            var mode = groups.Where(g => g.Count() > maxCount-10).Select(g => g.Key);
+            mode=mode.Select(m=>m-1).Union(mode.Select(m => m+1)).Union(mode);
+
+            int sum = int.MaxValue;
+            foreach(var m in mode){
+                sum=Math.Min(sum,data.Sum(a => calcPrice(Math.Abs(m-a))));
+            }
+            return sum;
+        }
+
+        int calcPrice(int distance){
+            // S(n)=1+2+...+n=n(n+1)/2
+            return distance*(distance+1)/2;
         }
     }
 }
--- day07/GiantWhaleTests/CrabTest.cs
+++ day07/GiantWhaleTests/CrabTest.cs
@@ -16,5 +16,15 @@ namespace Day07.GiantWhaleTests
             var actual = new GiantWhale.Crab(data).Solve();
             Assert.Equal(expected, actual);
         }
+
+        [Theory]
+        [InlineData("test.txt", 168)]
+        [InlineData("input.txt", 96708205)]
+        public void IncreasingFuelTest(string file, int expected)
+        {
+            var data = File.ReadAllText(file).Split(',').Select(int.Parse).ToArray();
+            var actual = new GiantWhale.Crab(data).IncreasingPriceSolve();
+            Assert.Equal(expected, actual);
+        }
     }
 }
