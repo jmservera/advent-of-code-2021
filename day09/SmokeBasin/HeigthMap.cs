0401e057da49a262f86b1a701b9c27ae0824e5e0
Juan Manuel Servera <8036360+jmservera@users.noreply.github.com>
2021-12-15 14:30:57 +0000

    day09 solved

--- day09/SmokeBasin/HeigthMap.cs
+++ day09/SmokeBasin/HeigthMap.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Day09.SmokeBasin
 {
@@ -10,7 +12,79 @@ namespace Day09.SmokeBasin
             this.map=data;
         }
         public int CalcRisk(){
-            return 0;
+            var lowPoints=findLowPoints();
+            int result=0;
+            foreach(var point in lowPoints){
+                
+                result+=1+map[point.Item1,point.Item2];
+            }
+            return result;
+        }
+
+        public int CalcBasins(){
+            var lowPoints=findLowPoints();
+            List<int> basins=new List<int>();
+            foreach(var point in lowPoints){
+                basins.Add(measureBasin(point.Item1,point.Item2));
+            }
+
+            basins.Sort();
+            basins.Reverse();
+            return basins.Take(3).Aggregate((x,y)=>x*y);
+        }
+
+        private List<Tuple<int,int>> findLowPoints(){
+            var lowPoints=new List<Tuple<int,int>>();
+            for(int i=0;i<map.GetLength(0);i++){
+                for(int j=0;j<map.GetLength(1);j++){
+                    if(isLowPoint(i,j)){
+                        lowPoints.Add(new Tuple<int,int>(i,j));
+                    }
+                }
+            }
+            return lowPoints;
+        }
+
+        bool isLowPoint(int i, int j){
+            for(int x=i-1;x<=i+1;x++){
+                for(int y=j-1;y<=j+1;y++){
+                    if(x==i && y==j){
+                        continue;
+                    }
+                    if(x>=0 && x<map.GetLength(0) && y>=0 && y<map.GetLength(1)){
+                        if(map[x,y]<=map[i,j]){
+                            return false;
+                        }
+                    }
+                }
+            }
+            return true;
+        }
+
+        int measureBasin(int i, int j){
+            List<Tuple<int,int>> basinPoints=new List<Tuple<int,int>>();
+            
+            fillBasinPoints(i,j,basinPoints);
+            return basinPoints.Count;
+        }
+
+        void fillBasinPoints(int i, int j, List<Tuple<int,int>> basinPoints){
+            if(i<0 || i>=map.GetLength(0) || j<0 || j>=map.GetLength(1)){
+                return;
+            }
+            if(map[i,j]==9){
+                return;
+            }
+            else{
+                if(basinPoints.Contains(new Tuple<int,int>(i,j))){
+                    return;
+                }
+                basinPoints.Add(new Tuple<int,int>(i,j));
+                fillBasinPoints(i-1,j,basinPoints);
+                fillBasinPoints(i+1,j,basinPoints);
+                fillBasinPoints(i,j-1,basinPoints);
+                fillBasinPoints(i,j+1,basinPoints);
+            }
         }
     }
 }
--- day09/SmokeBasinTests/HeightMapTest.cs
+++ day09/SmokeBasinTests/HeightMapTest.cs
@@ -9,11 +9,23 @@ namespace Day09.SmokeBasinTests
     {
         [Theory]
         [InlineData("test.txt",15)]
+        [InlineData("input.txt",548)]
         public void TestRisk(string fileName,int expected)
         {
             var data = DataLoader.LoadData(fileName);
             var map = new HeigthMap(data);
             var actual = map.CalcRisk();
+            Assert.Equal(expected,actual);
+        }
+
+        [Theory]
+        [InlineData("test.txt",1134)]
+        [InlineData("input.txt",786048)]
+        public void TestBasins(string fileName,int expected)
+        {
+            var data = DataLoader.LoadData(fileName);
+            var map = new HeigthMap(data);
+            var actual = map.CalcBasins();
             Assert.Equal(expected,actual);
         }
     }
