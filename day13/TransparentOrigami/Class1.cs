a6d1e052e408869eeea6bef54ae7144f195bc21a
Juan Manuel Servera <8036360+jmservera@users.noreply.github.com>
2021-12-16 23:42:36 +0000

    Add day 13

---
+++ day13/TransparentOrigami/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace TransparentOrigami
+{
+    public class Class1
+    {
+    }
+}
---
+++ day13/TransparentOrigamiTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace TransparentOrigamiTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
